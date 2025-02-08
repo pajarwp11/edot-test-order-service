53d0137288c2aa55261294091d72df1ab078cce7
Pajar Wijaya Putra <pajar.putra.aux@flip.id>
2025-02-08 14:04:33 +0700

    add cart repo

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/gorilla/mux v1.8.1
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/redis/go-redis v6.15.9+incompatible
 	github.com/redis/go-redis/v9 v9.7.0
 )
 
@@ -13,4 +14,7 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.36.2 // indirect
 )
---
+++ repository/cart/cart.go
@@ -0,0 +1,47 @@
+package cart
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"order-service/models/cart"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type CartRepository struct {
+	redisClient *redis.Client
+}
+
+func NewCartRepository(redis *redis.Client) *CartRepository {
+	return &CartRepository{
+		redisClient: redis,
+	}
+}
+
+func (c *CartRepository) Insert(cart *cart.Cart) error {
+	cartByte, err := json.Marshal(cart)
+	if err != nil {
+		return err
+	}
+	key := fmt.Sprintf("cart:%d", cart.UserId)
+	ctx := context.Background()
+	err = c.redisClient.Set(ctx, key, string(cartByte), 30*24*time.Hour).Err()
+	return err
+}
+
+func (c *CartRepository) Get(userId int) (*cart.Cart, error) {
+	key := fmt.Sprintf("cart:%d", userId)
+	ctx := context.Background()
+	cartData, err := c.redisClient.Get(ctx, key).Result()
+	if err != nil {
+		return nil, err
+	}
+	cart := cart.Cart{}
+	err = json.Unmarshal([]byte(cartData), &cart)
+	if err != nil {
+		return nil, err
+	}
+	return &cart, nil
+}
